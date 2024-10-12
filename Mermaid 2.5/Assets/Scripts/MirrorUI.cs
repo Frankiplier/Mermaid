f45e55b522eff9d0b00d58b80781d1c7136691e2
Frankiplier <agatarucka@gmail.com>
2024-10-13 01:39:40 +0200

    UI picking mirror pieces

---
+++ Mermaid 2.5/Assets/Scripts/MirrorUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorUI : MonoBehaviour
+{
+    [SerializeField] MirrorInfo pickedPieces;
+    [SerializeField] int index;
+    public GameObject piece;
+
+    void Start()
+    {
+        piece.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (pickedPieces.pickedUpPieces[index] == true)
+        {
+           piece.SetActive(true);
+        }
+    }
+}
